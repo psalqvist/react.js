0a940472d1957137eecbcbf2e94a1eaee283218b
Philip Salqvist <philipsalqvist@MacBook-Pro-som-tillhor-Philip.local>
2022-03-09 20:26:02 +0100

    Init react redux project

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjskCbxzPzSrG1ZmEuYEX5ICR2IQ-BOHE",
+    authDomain: "netflix-ad7f3.firebaseapp.com",
+    projectId: "netflix-ad7f3",
+    storageBucket: "netflix-ad7f3.appspot.com",
+    messagingSenderId: "501458852788",
+    appId: "1:501458852788:web:0e12b309a4a1ff541ef0e1"
+  };
